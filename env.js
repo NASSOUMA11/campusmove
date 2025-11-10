2ec74eabc71e2117a9e06aa24f3ca7acad3d97a8
NASSOUMA11 <nassouma@hestim.ma>
2025-11-10 20:13:53 +0100

    Create env.js

---
+++ env.js
@@ -0,0 +1,4 @@
+<script>
+  window.SUPABASE_URL = "https://zomvqsnmdvqjaqsflhss.supabase.co";
+  window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbXZxc25tZHZxamFxc2ZsaHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTQyMTUsImV4cCI6MjA3ODM3MDIxNX0.D7ZLth2kpZZIc1XX9_myUluut0fGrdQ6FMWEr799uw8";
+</script>
